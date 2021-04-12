b8c26f0bbb8bab4b02b981fa297fcb26bc0b1443
Matheus <mmathias2011@outlook.com>
2021-04-11 23:30:21 -0300

    Adicionado animação para arrows do carrossel e estrutura inicial do footer

--- scripts/carrossel.js
+++ scripts/carrossel.js
@@ -1,29 +1,4 @@
-/* $(function(){
 
-    function atualizar_info () {
-
-        $(".product-name").text( $(".slick-center").data("nome"));
-        var productPrice = parseFloat($(".slick-center").data("valor")).toLocaleString("pt-BR,", {style:"currency", currency:"BRL", minimumFractionDigits: 2});
-        $(".product-price").text(productPrice);
-    }
-    $(".carrossel-mais-vendidos").on("init", function(){
-        atualizar_info();
-    })
-
-    $(".carrossel-mais-vendidos").slick({
-        autoplay: true,
-        infinite: true,
-        arrows: true,
-        slidesToShow: 3,
-        slidesToScroll: 1,
-        centerMode: true,
-        autoplaySpeed: 1800,   
-    });
-
-    $(".carrossel-mais-vendidos").on("afterChange", function(){
-        atualizar_info();
-    })
-}); */
 
 $(function(){
 
